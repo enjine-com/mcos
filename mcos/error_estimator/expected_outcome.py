6480cb499024e3d8fb45e7fd7268db2ba9ec11b2
Andrew Stanley <me@andrewstanley.ca>
2020-01-26 19:23:52 -0500

    Updated error estimators to use optimal allocation

--- mcos/error_estimator/expected_outcome.py
+++ mcos/error_estimator/expected_outcome.py
@@ -5,8 +5,7 @@ from mcos.error_estimator import AbstractErrorEstimator
 
 class ExpectedOutcomeErrorEstimator(AbstractErrorEstimator):
 
-    def estimate(self, allocation: np.array) -> float:
-        true_allocation = np.repeat(allocation.T, 1, axis=0)
-        estimation = (true_allocation - allocation).mean()
+    def estimate(self, allocation: np.array, optimal_allocation: np.array) -> float:
+        optimal_allocation = np.repeat(optimal_allocation.T, 1, axis=0)
+        estimation = (optimal_allocation - allocation).mean()
         return estimation
-        
--- mcos/mcos.py
+++ mcos/mcos.py
@@ -1,14 +1,14 @@
 from typing import List
 
-from mcos.error_estimator.base import ErrorEstimator
+from mcos.error_estimator import AbstractErrorEstimator
 from mcos.observation_simulator.base import ObservationSimulator
-from mcos.optimizer.base import Optimizer
+from mcos.optimizer import AbstractOptimizer
 import numpy as np
 import pandas as pd
 
 
-def simulate_optimizations(obs_simulator: ObservationSimulator, n_sims: int, optimizers: List[Optimizer],
-             error_estimator: ErrorEstimator, de_noise: bool = True) -> pd.DataFrame:
+def simulate_optimizations(obs_simulator: ObservationSimulator, n_sims: int, optimizers: List[AbstractOptimizer],
+             error_estimator: AbstractErrorEstimator, de_noise: bool = True) -> pd.DataFrame:
     error_estimates = {optimizer.name: [] for optimizer in optimizers}
 
     for i in range(n_sims):
@@ -19,7 +19,8 @@ def simulate_optimizations(obs_simulator: ObservationSimulator, n_sims: int, opt
 
         for optimizer in optimizers:
             allocation = optimizer.allocate(mu_hat, cov_hat)
-            error_estimates[optimizer.name].append(error_estimator.estimate(allocation))
+            optimal = optimal_allocation(mu_hat, cov_hat)
+            error_estimates[optimizer.name].append(error_estimator.estimate(allocation, optimal))
 
     return pd.DataFrame([
         {
@@ -28,3 +29,11 @@ def simulate_optimizations(obs_simulator: ObservationSimulator, n_sims: int, opt
             'stdev': np.std(error_estimator[optimizers.name])
         } for optimizer in optimizers
     ]).set_index('optimizer')
+
+
+def optimal_allocation(mu: np.array, cov: np.array):
+    inv = np.linalg.inv(cov)
+    ones = np.ones(shape=(inv.shape[0],1))
+    allocation = np.dot(inv,mu)
+    allocation /= np.dot(ones.T, allocation)
+    return allocation
