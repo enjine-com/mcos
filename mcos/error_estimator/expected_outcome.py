bbf38b72ab95a3d1d4f7a5894555808264cad842
Andrew Stanley <me@andrewstanley.ca>
2020-01-27 20:20:57 -0500

    Updated docstrings

--- mcos/error_estimator/expected_outcome.py
+++ mcos/error_estimator/expected_outcome.py
@@ -4,6 +4,7 @@ from mcos.error_estimator import AbstractErrorEstimator
 
 
 class ExpectedOutcomeErrorEstimator(AbstractErrorEstimator):
+    """Error Estimator that calculates the mean difference in expected outcomes"""
 
     def estimate(self, allocation: np.array, optimal_allocation: np.array) -> float:
         return (optimal_allocation - allocation).mean()
