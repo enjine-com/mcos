6866c4a81fb9ab4c0d3342007341b7746c7b1814
Andrew Stanley <me@andrewstanley.ca>
2020-02-03 14:46:56 -0500

    Changed calculation order

--- mcos/error_estimator.py
+++ mcos/error_estimator.py
@@ -29,4 +29,4 @@ class VarianceErrorEstimator(AbstractErrorEstimator):
     """Error Estimator that calculates the mean difference in variance"""
 
     def estimate(self, mu: np.array, cov: np.array, allocation: np.array, optimal_allocation: np.array) -> float:
-        return np.dot(optimal_allocation - allocation, np.dot(optimal_allocation - allocation, cov))
+        return np.dot(optimal_allocation - allocation, np.dot(cov, optimal_allocation - allocation))
