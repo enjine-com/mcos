a0db74a8ac77a5bcce8f755bb744f3d4355f5ba5
Owen Christie <owendot2011@gmail.com>
2020-02-11 15:52:42 -0500

    moved de-noising folder inside mcos module, added boilerplate for tests and doc strings

--- mcos/de-noising/de-noise-conv.py
+++ mcos/de-noising/de-noise-conv.py
@@ -5,6 +5,14 @@ import pandas as pd
 
 
 def fit_KDE(obs: np.array, b_width: float = .25, kernel: str = 'gaussian', x: np.array = None) -> pd.Series:
+    """
+
+    :param obs:
+    :param b_width:
+    :param kernel:
+    :param x:
+    :return:
+    """
     # Fit kernel to a series of obs, and derive the prob of obs
     # x is the array of values on which the fit KDE will be evaluated
     if len(obs.shape) == 1:
@@ -20,6 +28,13 @@ def fit_KDE(obs: np.array, b_width: float = .25, kernel: str = 'gaussian', x: np
 
 
 def mp_PDF(var: float, q: float, pts: int) -> pd.Series:
+    """
+
+    :param var:
+    :param q:
+    :param pts:
+    :return:
+    """
     # Marcenko-Pastur pdf
     # q=T/N
     e_min, e_max = var * (1 - (1. / q) ** .5) ** 2, var * (1 + (1. / q) ** .5) ** 2
@@ -30,6 +45,15 @@ def mp_PDF(var: float, q: float, pts: int) -> pd.Series:
 
 
 def err_PDFs(var: float, e_val: pd.Series, q: float, b_width: float, pts: int = 1000) -> float:
+    """
+
+    :param var:
+    :param e_val:
+    :param q:
+    :param b_width:
+    :param pts:
+    :return:
+    """
     # Fit error
     pdf0 = mp_PDF(var, q, pts)  # theoretical pdf
     pdf1 = fit_KDE(e_val, b_width, x=pdf0.index.values)  # empirical pdf
@@ -38,6 +62,13 @@ def err_PDFs(var: float, e_val: pd.Series, q: float, b_width: float, pts: int = 
 
 
 def find_max_eval(e_val: np.array, q: float, b_width: float) -> (float, float):
+    """
+
+    :param e_val:
+    :param q:
+    :param b_width:
+    :return:
+    """
     # Find max random e_val by fitting Marcenko's dist to the empirical one
     out = minimize(
         lambda *x: err_PDFs(*x),
@@ -54,11 +85,22 @@ def find_max_eval(e_val: np.array, q: float, b_width: float) -> (float, float):
 
 
 def corr_to_cov(corr: np.array, std: np.array) -> np.array:
+    """
+
+    :param corr:
+    :param std:
+    :return:
+    """
     cov = corr * np.outer(std, std)
     return cov
 
 
 def cov_to_corr(cov: np.array) -> np.array:
+    """
+
+    :param cov:
+    :return:
+    """
     # Derive the correlation matrix from a covariance matrix
     std = np.sqrt(np.diag(cov))
     corr = cov / np.outer(std, std)
@@ -67,6 +109,11 @@ def cov_to_corr(cov: np.array) -> np.array:
 
 
 def get_PCA(matrix: np.array) -> (np.array, np.array):
+    """
+
+    :param matrix:
+    :return:
+    """
     # Get e_val,e_vec from a Hermitian matrix
     e_val, e_vec = np.linalg.eigh(matrix)
     indices = e_val.argsort()[::-1]  # arguments for sorting e_val desc
@@ -76,6 +123,13 @@ def get_PCA(matrix: np.array) -> (np.array, np.array):
 
 
 def denoised_corr(e_val: np.array, e_vec: np.array, n_facts: int) -> np.array:
+    """
+
+    :param e_val:
+    :param e_vec:
+    :param n_facts:
+    :return:
+    """
     # Remove noise from corr by fixing random eigenvalues
     e_val_ = np.diag(e_val).copy()
     e_val_[n_facts:] = e_val_[n_facts:].sum() / float(e_val_.shape[0] - n_facts)
@@ -86,6 +140,13 @@ def denoised_corr(e_val: np.array, e_vec: np.array, n_facts: int) -> np.array:
 
 
 def de_noise_cov(cov0: np.array, q: float, b_width: float) -> np.array:
+    """
+
+    :param cov0:
+    :param q:
+    :param b_width:
+    :return:
+    """
     corr0 = cov_to_corr(cov0)
     e_val_0, e_vec_0 = get_PCA(corr0)
     e_max_0, var0 = find_max_eval(np.diag(e_val_0), q, b_width)
---
+++ tests/de-noising/test_denoise_conv.py
@@ -0,0 +1,27 @@
+class TestDeNoiseConv:
+    def test_fit_KDE(self):
+        pass
+
+    def test_mp_PDF(self):
+        pass
+
+    def test_err_PDFs(self):
+        pass
+
+    def test_find_max_eval(self):
+        pass
+
+    def test_corr_to_cov(self):
+        pass
+
+    def test_cov_to_corr(self):
+        pass
+
+    def test_get_PCA(self):
+        pass
+
+    def test_denoised_corr(self):
+        pass
+
+    def test_de_noise_cov(self):
+        pass
