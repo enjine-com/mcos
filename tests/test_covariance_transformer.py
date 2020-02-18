7168f0bc01a835347a4e15ef3b6c9ff0001438c5
Owen Christie <owendot2011@gmail.com>
2020-02-18 10:31:52 -0500

    added test for now public cov_to_corr

--- tests/test_covariance_transformer.py
+++ tests/test_covariance_transformer.py
@@ -1,4 +1,4 @@
-from mcos.covariance_transformer import CovarianceMatrixDeNoiser
+from mcos.covariance_transformer import CovarianceMatrixDeNoiser, cov_to_corr
 from numpy.testing import assert_almost_equal
 from pypfopt.risk_models import sample_cov
 
@@ -10,3 +10,11 @@ class TestCovarianceMatrixDeNoiser:
         results = CovarianceMatrixDeNoiser(covariance_matrix, n_observations).transform()
         assert results.shape == (20, 20)
         assert_almost_equal(results, de_noised_covariance_matrix_results)
+
+
+def test_cov_to_corr(prices_df):
+    covariance_matrix = sample_cov(prices_df).values
+    correlation_matrix = cov_to_corr(covariance_matrix)
+    assert correlation_matrix.shape == covariance_matrix.shape
+    assert correlation_matrix.max() <= 1
+    assert correlation_matrix.min() >= -1
