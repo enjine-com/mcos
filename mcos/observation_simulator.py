bbca0415e9b051e895b62bd14771992781bf030b
= <=>
2020-04-14 11:40:16 -0400

    dev work

--- mcos/observation_simulator.py
+++ mcos/observation_simulator.py
@@ -51,19 +51,26 @@ class MuCovJackknifeObservationSimulator(AbstractObservationSimulator):
         self.jackknife_samples = jackknife_samples
 
     def simulate(self) -> (np.array, np.array):
-        samples = pd.DataFrame()
-        for q in range(self.jackknife_samples):
-            x = np.random.multivariate_normal(self.mu.flatten(), self.cov, size=self.n_observations)
-            x_prime = pd.DataFrame()
-            mu_prime = []
-            for count in range(np.size(x, 1)):
-                y = x[:,count]
-                y_prime = []
-                for z in y:
-                    y_prime.append((sum(y) - z) / (self.n_observations - 1))
-                x_prime = x_prime.append([y_prime])
-                mu_prime.append(np.asarray(y_prime).mean())
-            samples = samples.append([mu_prime])
 
-        return np.asarray(mu_prime).reshape(-1, 1), sample_cov(x_prime)
+        x = {}
+        for q in range(self.jackknife_samples):
+            x[q] = np.random.multivariate_normal(self.mu.flatten(), self.cov, size=self.n_observations)
+
+        x_prime = None
+        for count in range(x.__len__()):
+            x_total = None
+            for y in range(x.__len__()):
+                if count != y:
+                    if x_total is None:
+                        x_total = x[y]
+                    else:
+                        x_total = x_total + x[y]
+            x_total = x_total / (x.__len__() - 1)
+            if x_prime is None:
+                x_prime = x_total
+            else:
+                x_prime = x_prime + x_total
+        x_prime = x_prime / x.__len__()
+
+        return x_prime.mean(axis=0).reshape(-1, 1), np.cov(x_prime, rowvar=False)
 
